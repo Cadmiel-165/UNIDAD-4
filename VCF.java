25a6f48d876dbb77b927e668b74e8ff666dfb1b7
Cadmiel-165 <25680165@cuautla.tecnm.mx>
2025-12-09 23:55:44 -0600

    Conversor de Temperatura VCF
    Instrucción: Convertir valores entre Celsius y Fahrenheit usando fórmulas matemáticas.

--- VCF.java
+++ VCF.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
 public class VCF {
     public static void main(String[] args) {
@@ -36,5 +36,6 @@ public class VCF {
         sc.close();
     }
 }
+
 
 
